71a038546284ed66ecd9398d0968d5c80f7b2bab
Pravz <pravallikan@finfriday.com>
2019-02-05 17:13:10 +0530

    added nabar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
 import logo from '../q.svg'
+import styled from 'styled-components'
+
  
 export default class Navbar extends Component {
 
+  // const NavWrapper = styled.nav `
+  //   background: var(--mainBlue);
+  //   .nav-link{
+  //       color:var(--mainWhite)!important;
+  //       font-size:1.3rem;
+  //       text-transform:capitalize;
+  //   }`
+
   render() {
+
+    
     
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark px-sm-5" >
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
       {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
         Creative Commons (Attribution 3.0 Unported);
         https://www.iconfinder.com/Makoto_msk  */}
@@ -27,14 +39,48 @@ export default class Navbar extends Component {
 
         </ul>
         <Link to= "/cart" className="ml-auto">
-          <button>
+          <ButtonContainer>
             <i className = "fas fa-cart-plus"/>
-            my cart
-          </button> 
+            My Cart
+          </ButtonContainer> 
         </Link>
-      </nav>
+        </NavWrapper>
     )
   
   }
+
+  
 }
+
+const ButtonContainer = styled.button`
+  text-transform: capitalize;
+  font-size: 1.4rem;
+  background: transparent;
+  border: 0.5 rem solidv var(--lightBlue);
+  color:var(--black);
+  border-radius:0.5rem;
+  paddding: 0.2rem 0.5rem;
+  cursor:pointer;
+  margin:0.2rem 0.5 rem 0.2rem 0;
+  transition:all 0.5s ease-in-out;
+  &:hover {
+    background:var(--white);
+    color:var(--mainBlue);
+  }
+  &:focus{
+    outline:none;
+  }
+`;
+
+const NavWrapper = styled.nav `
+ background: var(--green);
+ height:85px;
+ .nav-link{
+     color:var(--mainWhite)!important;
+     font-size:1.3rem;
+     text-transform:capitalize;
+ }
+ `
+
+
 
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
 import Product from './Product'
+import Title from './Title';
 export default class ProductList extends Component {
+
+  // constructor()
+  // {
+      
+
+  //     this.state = {
+  //       products:[]
+  //     }
+  // }
   render() {
+    
     return (
-      <div>
-         <Product/>
+      <React.Fragment>
+      <div className ="py-5">
+      <div className ="container">
+         <Title name ="our" title ="vegetables" />
+      <div className ="row">
       </div>
+      </div>
+      </div>
+      </React.Fragment>
     )
   }
 }
