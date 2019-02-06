c9607545c8506b90ae324df2c660a2aa5c66d86c
Mayank Sharma <pravallikan@finfriday.com>
2019-02-06 22:07:09 +0530

    dtails page

--- src/components/Details.js
+++ src/components/Details.js
@@ -19,68 +19,87 @@ export default class Details extends Component {
             inCart
           } = value.detailProduct;
           return (
-            <div className = "container py-5">
-          {/* ---------title---------------- */}
-              <div className = "row">
-                  <div className ="col-10 mx-auto text-center text -slanted text-blue my-5">
-                  <h1>{title}</h1>
-                  </div>
-              </div>
-            {/* ---------------- end title ------------- */}
-          {/* ------------------product info ------------ */}
-              <div className ="col-10 mx-auto col-md-6 my-3">
-              </div>
-              <img src = {img} className = "img-fluid" alt = "product" />
-              {/* ----------------product text -------------- */}
-              <div className ="col-10 mx-auto col-md-6 my-3 
-              text-capitalize">
-              <h2>Details : {title} </h2>
-              <h4 className = "text-title text-uppercase text-muted mt-3 mb-2"> 
-               made by : <span className ="text-uppercase">
-               {company} </span>
-              </h4>
-              <h4 className = "text-green">
-                <strong>
-                  price: <span>Rs.</span> 
-                  {price}
-                </strong>
-              </h4>
-              <p className ="text-capitalize font-weight-bold mt-3 mb-0">
-              some info about product:
-              </p>
-              <p className="text-muted lead">{info}</p>
-              {/* --button-- */}
-              <Link to = "/" >
-                <ButtonContainer>back to 
-                  products
-                </ButtonContainer>
-              </Link>
-              </div>
-          </div>
-          )
-        }}
+            <div className="container py-5">
+              <div className="row">
+                  <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                    <h1>{title}</h1>
+                    </div>
+                    </div>
+                      <div className="row">
+                        <div className="col-10 mx-auto col-md-6 my-3">
+                            <img src={img} className='img-fluid' alt="product" />
+                        </div>
+
+
+                        <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+
+                        <h2>details:{title}</h2>
+
+                        <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                        made by: <span className="text-uppercase">
+                        {company}</span>
+                        </h4>
+
+                        <h4 className="text-green">
+                        <strong>
+                            price:<span>Rs</span>
+                            {price}
+                        </strong>
+                        </h4>
+
+                        <p className="text-capitalize font-weight font-weight-bold mt-3 mb-0">
+                        some info about vegetables:
+                        </p>
+
+                        <p className="text-muted lead">{info}</p>
+
+                        <div>
+                            <Link to="/">
+                            <ButtonContainer>
+                                back to vegetables
+                            </ButtonContainer>
+                            </Link>
+
+                            <ButtonContainer 
+                            cart
+                            disabled={inCart?true:false}
+                            onClick={() => {
+                                value.addToCart(id)
+                                value.handleDetails(id)
+                            }}
+                            >
+                                {inCart?'inCart':'add to cart'}
+                            </ButtonContainer>
+
+
+                        </div>
+                      </div>
+                  </div>       
+            </div>
+        )
+        
+}}
       </ProductConsumer>
     )
   }
 }
 
-
 const ButtonContainer = styled.button`
-  text-transform: capitalize;
-  font-size: 1.4rem;
-  background: transparent;
-  border: 0.5 rem solidv var(--lightBlue);
-  color:var(--black);
-  border-radius:0.5rem;
-  paddding: 0.2rem 0.5rem;
-  cursor:pointer;
-  margin:0.2rem 0.5 rem 0.2rem 0;
-  transition:all 0.5s ease-in-out;
-  &:hover {
-    background:var(--white);
+text-transform:capitalize;
+font-size: 1.4rem;
+background: transparent;
+border: 0.05rem solid var(--lightBlue);
+border-color:${props=>props.cart?"var(--mainYellow)":"var(--lightBlue)"};
+color: ${prop=>prop.cart?"var(--mainYellow)":"var(--lightBlue)"};
+border-radius: 0.5rem;
+padding:0.2rem 0.5rem;
+cursor:pointer;
+margin:0.2rem 0.5rem 0.2rem 0;
+transition:all 0.5s ease-in-out;
+&:hover{
+    background:${prop => prop.cart?"var(--mainYellow)":"var(--lightBlue)"};
     color:var(--mainBlue);
-  }
-  &:focus{
+}
+&:focus{
     outline:none;
-  }
-`;
+}`
